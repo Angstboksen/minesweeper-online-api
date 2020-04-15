464929f80d37cb0faf33fc6ad30110f5755c7c18
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-04-16 01:09:39 +0200

    edited user passwords

--- minesweeperapi/api/models.py
+++ minesweeperapi/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class MinesweeperUser(models.Model):
-    username = models.CharField(max_length=150)
+    username = models.CharField(unique=True, max_length=150)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
     email = models.EmailField(max_length=70, unique=True)
@@ -17,7 +17,7 @@ class MinesweeperUser(models.Model):
         if is_new:
             authuser = User(username=self.username, email=self.email,
                             first_name=self.first_name, last_name=self.last_name)
-            authuser.set_password(self.email)
+            authuser.set_password(self.username)
             authuser.save()
 
     class Meta:
