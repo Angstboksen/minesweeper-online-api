3fdce613fd141d164e34df1d43a1855be7cf3e4b
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-05-14 13:46:49 +0200

    added static root

--- minesweeperapi/minesweeperapi/settings.py
+++ minesweeperapi/minesweeperapi/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
